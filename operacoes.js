18fc88579e5bb2a923c52f87f529d6bd0fd229f3
henriquejohonson <80105691+henriquejohonson@users.noreply.github.com>
2021-04-15 23:45:42 -0300

    Add files via upload

---
+++ operacoes.js
@@ -0,0 +1,15 @@
+var operacoes = {
+    mais : (numA, numB) => {
+        return (numA + numB)
+    },
+    menos: (numA, numB) => {
+        return (numA - numB)
+    },
+    vezes: (numA, numB) => {
+        return (numA * numB)
+    },
+    divisao: (numA, numB) => {
+        return (numA / numB)
+    },  
+}
+module.exports = operacoes
